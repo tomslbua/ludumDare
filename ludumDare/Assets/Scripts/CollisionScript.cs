66995f26d96c17ab98900af5656c5638e9dbe124
tomslbua <50045165+tomslbua@users.noreply.github.com>
2019-04-28 14:36:21 +0200

    VG

---
+++ ludumDare/Assets/Scripts/CollisionScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionScript : MonoBehaviour
+{
+    public GameObject sphere;
+    SphereCollider sc;
+    bool script=true;
+    private void Start()
+    {
+        sc = sphere.GetComponent<SphereCollider>();
+        
+    }
+
+
+    void OnTriggerEnter(Collider other) {
+
+        if (script == true) { 
+
+        if (other.gameObject.tag == "Objects")
+        {
+
+            Vector3 onPlanet = Random.onUnitSphere * (sc.radius * transform.localScale.x + 6);
+            transform.position = onPlanet;
+            transform.LookAt(this.transform.position);
+            transform.rotation = transform.rotation * Quaternion.Euler(-90, 0, 90);
+
+            Debug.Log("trigger enter!");
+        }
+
+       else
+        {
+
+            gameObject.GetComponent<MeshRenderer>().enabled = true;
+            Debug.Log("funkt!");
+            script = false;
+            
+
+            
+
+            
+        }
+        }
+
+       
+        }
+    
+
+    
+
+
+
+}
--- ludumDare/Assets/Scripts/ObjectSpawner1.cs
+++ ludumDare/Assets/Scripts/ObjectSpawner1.cs
@@ -4,88 +4,90 @@ using UnityEngine;
 
 public class ObjectSpawner1 : MonoBehaviour
 {
-
-    //public GameObject[] objectsToSpawn;
-    //public float spawnRadius = 10.0f;
-    //public int numberOfObjects = 10;
-
-    //// Use this for initialization
-    //void Update()
-    //{
-    //    for (int i = 0; i < numberOfObjects; i++)
-    //    {
-    //        GameObject objectToSpawn = objectsToSpawn[Random.Range(0, objectsToSpawn.Length)];
-
-    //        Vector2 spawnPositionV2 = Random.insideUnitCircle * spawnRadius;
-
-    //        Vector3 spawnPosition = new Vector3(spawnPositionV2.x, 0.0f, spawnPositionV2.y);
-
-    //        Vector3 transformOffsetSpawnPosition = transform.position + spawnPosition;
-
-    //        RaycastHit hit;
-    //        if (Physics.Raycast(transformOffsetSpawnPosition, Vector3.down, out hit))
-    //        {
-
-    //            Vector3 finalSpawnPosition = hit.point;
-    //            Instantiate(objectToSpawn, finalSpawnPosition, Quaternion.identity);
-    //        }
-    //    }
-    //}
-
-    //    public int numObjects = 10;
-    //    public GameObject prefab;
-    //    public SphereCollider sc;
-
-    //    void Start()
-    //    {
-    //        sc = GetComponent<SphereCollider>();
-
-    //        Vector3 center = transform.position;
-    //        for (int i = 0; i < numObjects; i++)
-    //        {
-    //            Vector3 pos = RandomCircle(center, sc.radius);
-    //            Quaternion rot = Quaternion.FromToRotation(Vector3.forward, center +pos);
-
-    //            Instantiate(prefab, pos,rot);
-    //        }
-    //    }
-
-    //    Vector3 RandomCircle(Vector3 center, float radius)
-    //    {
-    //        //float ang = Random.value * 360;
-    //        Vector3 pos;
-
-    //        //pos.x = Random.Range(radius*transform.localScale.x, -radius * transform.localScale.x);
-    //        //pos.y = Random.Range(radius * transform.localScale.x, -radius * transform.localScale.x);
-    //        //pos.z = Random.Range(radius * transform.localScale.x, -radius * transform.localScale.x);
-    //        //pos.x = center.x + transform.localScale.x* radius * Mathf.Sin(ang * Mathf.Deg2Rad);
-    //        //pos.y = center.y + transform.localScale.x * radius * Mathf.Cos(ang * Mathf.Deg2Rad);
-    //        //pos.z = center.z;
-    //        return pos;
-    //    }
-    //}
     public float objNum;
-    public GameObject prefab;
+    public GameObject hochhaus;
     public SphereCollider sc;
-    
+    public GameObject fabrik;
+    public GameObject einfamilienhaus;
+    public GameObject supermarkt;
+
+
+    private void Awake()
+    {
+
+
+    }
 
     void Start()
     {
         sc = GetComponent<SphereCollider>();
+    }
 
-        for (int i = 0; i < objNum; i++)
-        {
+    public void Hochhaus()
+    {
+        
             Vector3 origin = this.transform.position / 0.5f;
 
             Vector3 onPlanet = Random.onUnitSphere * (sc.radius * transform.localScale.x + 6);
 
 
-            GameObject newGO = Instantiate(prefab, onPlanet, Quaternion.identity) as GameObject;
-            newGO.transform.SetParent(this.transform);
+            GameObject newGO = Instantiate(hochhaus, onPlanet, Quaternion.identity) as GameObject;
+            
+           
+
+
+
+        newGO.transform.SetParent(this.transform);
             newGO.transform.LookAt(this.transform.position);
             newGO.transform.rotation = newGO.transform.rotation * Quaternion.Euler(-90, 0, 90);
-            Debug.Log(sc.radius);
-        }
+            
+    }
+    public void Fabrik()
+    {
+
+        Vector3 origin = this.transform.position / 0.5f;
+
+        Vector3 onPlanet = Random.onUnitSphere * (sc.radius * transform.localScale.x-2.5f);
+
+
+        GameObject newGO = Instantiate(fabrik, onPlanet, Quaternion.identity) as GameObject;
+        newGO.transform.SetParent(this.transform);
+        newGO.transform.LookAt(this.transform.position);
+        newGO.transform.rotation = newGO.transform.rotation * Quaternion.Euler(0, 180, 0);
+        
+
+    }
+    public void Einfamilienhaus()
+    {
+
+        Vector3 origin = this.transform.position / 0.5f;
+
+        Vector3 onPlanet = Random.onUnitSphere * (sc.radius * transform.localScale.x + 6);
+
+
+        GameObject newGO = Instantiate(einfamilienhaus, onPlanet, Quaternion.identity) as GameObject;
+        newGO.transform.SetParent(this.transform);
+        newGO.transform.LookAt(this.transform.position);
+        newGO.transform.rotation = newGO.transform.rotation * Quaternion.Euler(-90, 0, 90);
+        
+        newGO.GetComponent<MeshRenderer>().enabled = false;
+
+    }
+
+    public void Supermarkt()
+    {
+
+        Vector3 origin = this.transform.position / 0.5f;
+
+        Vector3 onPlanet = Random.onUnitSphere * (sc.radius * transform.localScale.x + 6);
+
+
+        GameObject newGO = Instantiate(supermarkt, onPlanet, Quaternion.identity) as GameObject;
+        newGO.transform.SetParent(this.transform);
+        newGO.transform.LookAt(this.transform.position);
+        newGO.transform.rotation = newGO.transform.rotation * Quaternion.Euler(-90, 0, 90);
+       
+
     }
 
 }
