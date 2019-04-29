8960ed9c9742bdea5cc792521c517395d545b658
tomslbua <50045165+tomslbua@users.noreply.github.com>
2019-04-29 13:16:12 +0200

    menu

---
+++ ludumDare/Assets/Scripts/PLanetMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PLanetMenu : MonoBehaviour {
+
+  
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        transform.Rotate(0.5f, 0.5f, 0.5f);
+		
+	}
+}
